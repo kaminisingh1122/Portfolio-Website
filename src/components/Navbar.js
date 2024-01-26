b455479351ee7f9c66065dab73197378cef4acee
kamini singh <sengarkamini2002@gmail.com>
2024-01-26 23:27:18 +0530

    changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -104,7 +104,7 @@ function NavBar() {
 
             <Nav.Item className="fork-btn">
               <Button
-                href="https://github.com/soumyajit4419/Portfolio"
+                href="https://github.com/kaminisingh1122/Portfolio-Website"
                 target="_blank"
                 className="fork-btn-inner"
               >
