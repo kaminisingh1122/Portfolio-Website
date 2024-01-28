1c15b1db5222eca6d4f5f8865a8b593b280fe230
kamini singh <sengarkamini2002@gmail.com>
2024-01-28 18:10:38 +0530

    some changes

---
+++ src/components/About/AboutCard.js
@@ -0,0 +1,37 @@
+import React from "react";
+import Card from "react-bootstrap/Card";
+import { ImPointRight } from "react-icons/im";
+
+function AboutCard() {
+  return (
+    <Card className="quote-card-view">
+      <Card.Body>
+        <blockquote className="blockquote mb-0">
+          <p style={{ textAlign: "justify" }}>
+            Hi Everyone, I am <span className="purple">Kamini Singh </span>
+            from <span className="purple"> Hathras, India.</span>
+            <br />
+            I am persuing B.Tech with computer science and Engineering from Bundelkhand University, Jhansi
+            <br />
+           
+            <br />
+            <br />
+            Apart from coding, some other activities that I love to do!
+          </p>
+          <ul>
+            <li className="about-activity">
+              <ImPointRight /> Dancing
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Travelling
+            </li>
+          </ul>
+
+          
+        </blockquote>
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default AboutCard;
